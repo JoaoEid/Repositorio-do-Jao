76429f3982fed63aa2e46923c5557e23114524ac
JoaoEid <84354705+JoaoEid@users.noreply.github.com>
2021-09-09 17:33:00 -0300

    Update botoes.js

--- botoes.js
+++ botoes.js
@@ -1,30 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Formatação de texto com Java Script</title>
-  <style type="text/css">
-    h1 {
-      font-size: 20px;
-    }
-  </style>
-</head>
-<body>
-  <h1>Olá, pessoas. Sejam bem-vindas à minha página. É possível alterar o tamanho do texto utilizando os botões</h1>
-  <button onclick="+()">+</button>
-  <button onclick="-()">-</button>
-  <div class="btn-container">
-    <button name="increase-font" id="increase-font" title="aumentar fonte">+</button>
-    <button name="decrease-font" id="decrease-font" title="diminuir fonte">-</button>
-  </div>
-  <script> 
-    font.Size: 40px;
-    function +() {
-      document.querySelector('h1').style.fontSize = large;
-    }
-    
-    function -() {
-      documento.querySelector('h1').style.fontSize = medium;
-    }
-  </script>
-</body>
-</html>
+let tamanho = 2;
+function mudartamanho() {
+  h1 = document.querySelector("h1");
+  h1.style.fontSize = `${tamanho}em`;
+}
+function aumentartamanho() {
+  tamanho += 0.5;
+  mudartamanho();
+}
+function diminuirtamanho() {
+  if (tamanho > 0) {
+    tamanho -= 0.5;
+    mudartamanho();
+  }
+}
+function colocarBotoes() {
+  div = document.createElement("div")
+  div.innerHTML = ´
+    <button onclick="aumentartamanho()">+</button>
+    <button onclick="diminuirtamanho()">-</button>`
+  document.body.append(div);
+}
+
+   
+  
+
